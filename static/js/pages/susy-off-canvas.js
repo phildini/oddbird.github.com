e31428a61503894b4b01b1beb29fe9d08d7a1b11
Jonny Gerig Meyer <jonny@oddbird.net>
2012-11-27 16:09:37 -0700

    use jquery

---
+++ static/js/pages/susy-off-canvas.js
@@ -0,0 +1,31 @@
+var OBC = (function (OBC, $) {
+
+    'use strict';
+
+    OBC.susyOffCanvasToggle = {
+        init: function (triggers) {
+            $(triggers).click(function (e) {
+                e.preventDefault();
+                OBC.susyOffCanvasToggle.toggle(this);
+            });
+            return triggers;
+        },
+        toggle: function (el) {
+            var dir = $(el).attr('href');
+            if (dir === '#left') {
+                $('body').toggleClass('show-left').removeClass('show-right');
+            }
+            if (dir === '#right') {
+                $('body').toggleClass('show-right').removeClass('show-left');
+            }
+            return $('body').attr('class');
+        }
+    };
+
+    window.onload = function () {
+        OBC.susyOffCanvasToggle.init($('.toggle'));
+    };
+
+    return OBC;
+
+}(OBC || {}, jQuery));
